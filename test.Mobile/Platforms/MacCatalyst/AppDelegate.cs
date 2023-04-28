b9237f0f7f0306288ba350ed271e51aa958819c4
fabergiraldo <giraldo861@gmail.com>
2023-04-28 17:26:56 -0500

    Project Archicteture

---
+++ test.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace test.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
